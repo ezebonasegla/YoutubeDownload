cc0a9bdbac16f120e7b982160c2812c9606930fc
ezebonasegla <ebonasegla@gmail.com>
2022-08-17 15:06:14 -0300

    Primer Commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, Response, redirect, send_file
+import os
+from os import remove
+import pafy
+import moviepy.editor as mp
+
+app = Flask(__name__)
+path=os.getcwd() + '/static/'
+
+@app.route('/')
+def route():
+    return render_template('index.html')
+
+@app.route('/downloadMP4', methods=['GET', 'POST'])
+def downloadMP4():
+    if request.method == 'POST':
+        url = request.form.get("url")
+        video = pafy.new(url)
+        best = video.getbest(preftype="mp4")
+        best.download(path)
+        p = path + video.title + '.mp4'
+
+    return send_file(p, as_attachment=True)
+
+@app.route('/downloadMP3', methods=['GET', 'POST'])
+def downloadMP3():
+    if request.method == 'POST':
+        url = request.form.get("url")
+        video = pafy.new(url)
+        best = video.getbestaudio()
+        best.download()
+        p = path + video.title + '.m4a'
+
+    return send_file(p, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=True)
+
+
+
+
